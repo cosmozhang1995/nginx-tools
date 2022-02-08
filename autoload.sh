80896291ad907bf5f4c99f63b2c6f699f13b9c80
Cosmo Zhang <zjzxz3006@126.com>
2022-02-08 10:35:00 +0800

    fix

--- autoload.sh
+++ autoload.sh
@@ -2,10 +2,14 @@ export PATH=$NGINX_TOOLS_PATH:$PATH
 source $NGINX_TOOLS_PATH/env.sh
 
 list_available_sites() {
-	COMPREPLY=( $(ls $NGINX_CONFIG_PATH/sites-available) )
+	local filelist="$(ls $NGINX_CONFIG_PATH/sites-available)"
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	COMPREPLY=( $(compgen -W "$filelist" -- $cur) )
 }
 list_enabled_sites() {
-	COMPREPLY=( $(ls $NGINX_CONFIG_PATH/sites-enabled) )
+	local filelist="$(ls $NGINX_CONFIG_PATH/sites-enabled)"
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	COMPREPLY=( $(compgen -W "$filelist" -- $cur) )
 }
 
 complete -F list_available_sites ngensite
